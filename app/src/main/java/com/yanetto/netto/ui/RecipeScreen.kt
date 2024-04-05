483fbf90a2db656a359cfacfafc5e0118eba6916
yanetto <89203732802es@gmail.com>
2024-04-05 21:16:11 +0300

    - added TextFields

--- app/src/main/java/com/yanetto/netto/ui/RecipeScreen.kt
+++ app/src/main/java/com/yanetto/netto/ui/RecipeScreen.kt
@@ -1,8 +1,10 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.yanetto.netto.ui
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,12 +12,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedCard
@@ -28,8 +35,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -42,35 +52,51 @@ import com.yanetto.netto.ui.theme.NettoTheme
 fun RecipeScreen(
     modifier: Modifier = Modifier
 ){
-    Column (
+    LazyColumn (
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp)
+            //.verticalScroll(rememberScrollState())
+            .scrollable(
+                orientation = Orientation.Vertical,
+                state = rememberScrollState()
+            )
     ) {
-        Text(
-            text = "Sticky Red Wine Shallots With Oregano Polenta",
-            fontSize = 36.sp,
-            modifier = modifier
-                .padding(8.dp),
-            lineHeight = 40.sp
-        )
+        item{
+            Text(
+                text = "Sticky Red Wine Shallots With Oregano Polenta",
+                style = MaterialTheme.typography.displaySmall,
+                modifier = modifier
+                    .padding(8.dp)
+            )
 
-        Divider(
-            modifier = Modifier
+            Divider(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .width(1.dp)
+                    .padding(8.dp)
+            )
+
+            Text(
+                text = "DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription",
+                style = MaterialTheme.typography.titleLarge,
+                modifier = modifier
+                    .padding(8.dp)
+            )
+
+            ServingsCard(modifier = modifier)
+
+            Text(
+                text = "Ingredients",
+                style = MaterialTheme.typography.headlineMedium,
+                modifier = modifier.padding(top = 8.dp, bottom = 8.dp)
+            )
+
+            Divider(modifier = Modifier
                 .fillMaxWidth()
                 .width(1.dp)
-                .padding(8.dp)
-        )
-
-        Text(
-            text = "DescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescriptionDescription",
-            fontSize = 24.sp,
-            modifier = modifier
-                .padding(8.dp),
-            lineHeight = 28.sp
-        )
-
-        ServingsCard(modifier = modifier)
+            )
+        }
 
 //        Row(
 //            modifier = modifier
@@ -105,69 +131,193 @@ fun RecipeScreen(
 //                Text(text = stringResource(R.string.nutritional_info))
 //            }
 //        }
-        
 
-
-        LazyColumn(
-            modifier = modifier,
-            contentPadding = PaddingValues(4.dp),
-            verticalArrangement = Arrangement.spacedBy(4.dp)
-        ) {
-            item{
-                Text(text = "Ingredients", style = MaterialTheme.typography.headlineMedium)
-            }
-            items(4) {
-                IngredientItem(modifier)
-            }
+        items(15) {
+            IngredientItem(modifier)
         }
 
-        Row(
-            modifier = modifier
-                .padding(8.dp)
-        ) {
-            val colors = ButtonDefaults.outlinedButtonColors(
-                containerColor = MaterialTheme.colorScheme.surface,
-                contentColor = MaterialTheme.colorScheme.onSurface,
-                disabledContainerColor = MaterialTheme.colorScheme.primary,
-                disabledContentColor = MaterialTheme.colorScheme.onPrimary
+        item{
+            Text(
+                text = "Nutritional Info",
+                style = MaterialTheme.typography.headlineMedium,
+                modifier = modifier.padding(top = 8.dp)
             )
-            OutlinedButton(
-                onClick = {  },
-                colors = colors,
+
+            Row(
                 modifier = modifier
-                    .weight(1f, true)
+                    .padding(8.dp)
             ) {
-                Text(
-                    text = stringResource(R.string._100_g)
+                val colors = ButtonDefaults.outlinedButtonColors(
+                    containerColor = MaterialTheme.colorScheme.surface,
+                    contentColor = MaterialTheme.colorScheme.onSurface,
+                    disabledContainerColor = MaterialTheme.colorScheme.primary,
+                    disabledContentColor = MaterialTheme.colorScheme.onPrimary
                 )
+                OutlinedButton(
+                    onClick = {  },
+                    colors = colors,
+                    modifier = Modifier
+                        .weight(1f, true)
+                ) {
+                    Text(
+                        text = stringResource(R.string._100_g)
+                    )
+                }
+                Spacer(modifier = Modifier.weight(0.05f, true))
+                OutlinedButton(
+                    enabled = false,
+                    onClick = {  },
+                    modifier = Modifier
+                        .weight(1f, true),
+                    colors = colors
+                ) {
+                    Text(stringResource(R.string.serving))
+                }
+                Spacer(modifier = Modifier.weight(0.05f, true))
+                OutlinedButton(
+                    onClick = {  },
+                    modifier = Modifier
+                        .weight(1f, true),
+                    colors = colors,
+                ) {
+                    Text(stringResource(R.string.total))
+                }
             }
-            Spacer(modifier = modifier.weight(0.05f, true))
-            OutlinedButton(
-                enabled = false,
-                onClick = {  },
-                modifier = modifier
-                    .weight(1f, true),
-                colors = colors
-            ) {
-                Text(stringResource(R.string.serving))
-            }
-            Spacer(modifier = modifier.weight(0.05f, true))
-            OutlinedButton(
-                onClick = {  },
-                modifier = modifier
-                    .weight(1f, true),
-                colors = colors,
-            ) {
-                Text(stringResource(R.string.total))
-            }
+
+            NutritionalInfo(modifier = Modifier)
+
+            Spacer(modifier = Modifier.padding(4.dp))
+            PriceAndWeightLabels(label = stringResource(R.string.price), number = "100 rub", modifier = Modifier)
+            PriceAndWeightLabels(label = stringResource(R.string.weight), number = "200 g", modifier = Modifier)
         }
-        
-        
-        Spacer(modifier = modifier.padding(4.dp))
-        PriceAndWeightLabels(label = "Price:", number = "100 rub", modifier = modifier)
-        PriceAndWeightLabels(label = "Weight:", number = "200 g", modifier = modifier)
+
     }
 }
+
+@Composable
+fun NutritionalInfo(
+    modifier: Modifier = Modifier
+){
+    Divider(modifier = Modifier
+        .fillMaxWidth()
+        .width(1.dp)
+    )
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        Text(
+            text = "Energy",
+            textAlign = TextAlign.Start,
+            maxLines = 1,
+            overflow = TextOverflow.Clip
+        )
+
+        Spacer(modifier = Modifier.weight(1f, true))
+
+        Text(
+            modifier = Modifier,
+            text = "204 kcal",
+            textAlign = TextAlign.End
+        )
+    }
+    Divider(modifier = Modifier
+        .fillMaxWidth()
+        .width(1.dp)
+    )
+
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        Text(
+            text = "Protein",
+            textAlign = TextAlign.Start,
+            maxLines = 1,
+            overflow = TextOverflow.Clip
+        )
+
+
+        Spacer(modifier = Modifier.weight(1f, true))
+        Text(
+            modifier = Modifier,
+            text = "15 g",
+            textAlign = TextAlign.End
+        )
+    }
+    Divider(modifier = Modifier
+        .fillMaxWidth()
+        .width(1.dp)
+    )
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+
+        Text(
+            text = "Fat",
+            textAlign = TextAlign.Start,
+            maxLines = 1,
+            overflow = TextOverflow.Clip
+        )
+        Spacer(modifier = Modifier.size(4.dp))
+
+
+        Spacer(modifier = Modifier.weight(1f, true))
+        Text(
+            modifier = Modifier,
+            text = "8 g",
+            textAlign = TextAlign.End
+        )
+    }
+    Divider(modifier = Modifier
+        .fillMaxWidth()
+        .width(1.dp)
+    )
+
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+
+        Text(
+            text = "Carbohydrates",
+            textAlign = TextAlign.Start,
+            maxLines = 1,
+            overflow = TextOverflow.Clip
+        )
+        Spacer(modifier = Modifier.size(4.dp))
+
+
+        Spacer(modifier = Modifier.weight(1f, true))
+        Text(
+            modifier = Modifier,
+            text = "30 g",
+            textAlign = TextAlign.End
+        )
+    }
+    Divider(modifier = Modifier
+        .fillMaxWidth()
+        .width(1.dp)
+    )
+}
+
 
 @Composable
 fun PriceAndWeightLabels(
@@ -183,14 +333,14 @@ fun PriceAndWeightLabels(
     ) {
         Text(
             fontSize = 20.sp,
-            modifier = modifier,
+            modifier = Modifier,
             text = label,
             textAlign = TextAlign.Start
         )
         Spacer(modifier = modifier.weight(1f, true))
         Text(
             fontSize = 20.sp,
-            modifier = modifier,
+            modifier = Modifier,
             text = number,
             textAlign = TextAlign.End
         )
@@ -201,34 +351,56 @@ fun PriceAndWeightLabels(
 fun IngredientItem(
     modifier: Modifier = Modifier
 ){
+    val focusManager = LocalFocusManager.current
     Row(
         modifier = modifier
             .fillMaxWidth()
             .padding(8.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Column {
+        Column(modifier = Modifier.weight(2f)) {
             Text(
                 text = "Red wine",
                 textAlign = TextAlign.Start,
-                maxLines = 1,
-                overflow = TextOverflow.Clip
+                style = MaterialTheme.typography.titleMedium
             )
             Spacer(modifier = Modifier.size(4.dp))
             Text(
                 text = "description",
+                textAlign = TextAlign.Start,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
                 style = MaterialTheme.typography.labelLarge
             )
         }
 
-        Spacer(modifier = modifier.weight(1f, true))
+        Spacer(modifier = Modifier.size(16.dp))
+
+        var textValue by remember{mutableStateOf(TextFieldValue("25.5"))}
+
+        val color = if(textValue.text.toFloatOrNull() != null) MaterialTheme.colorScheme.onSurface else MaterialTheme.colorScheme.error
+
+
+        BasicTextField(
+            value = textValue,
+            //colors = TextFieldDefaults.outlinedTextFieldColors(
+                //focusedBorderColor = Color.Transparent,
+                //unfocusedBorderColor = Color.Transparent
+            //),
+            onValueChange = {textValue = it},
+            singleLine = true,
+            modifier = Modifier
+                .weight(1f),
+            textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.End, color = color),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
+            keyboardActions = KeyboardActions(onDone  = {focusManager.clearFocus()})
+        )
         Text(
-            modifier = modifier,
-            text = "93.5 g",
-            textAlign = TextAlign.End
+            text = "g",
+            modifier = Modifier.padding(4.dp)
         )
     }
-    Divider(modifier = Modifier
+    Divider(modifier = modifier
         .fillMaxWidth()
         .width(1.dp)
     )
@@ -244,18 +416,18 @@ fun ServingsCard(
             .padding(8.dp)
     ) {
         Row(
-            modifier = modifier,
+            modifier = Modifier,
             verticalAlignment = Alignment.CenterVertically
         ){
             IconButton(onClick = { /*TODO*/ },
-                modifier = modifier
+                modifier = Modifier
                     .padding(0.dp)
             ) {
 
                 Icon(
                     painter = painterResource(id = R.drawable.remove_circle_black_36dp),
                     contentDescription = null,
-                    modifier = modifier,
+                    modifier = Modifier,
                     tint = MaterialTheme.colorScheme.primary
                 )
             }
@@ -264,18 +436,18 @@ fun ServingsCard(
                 text = "3 Servings",
                 textAlign = TextAlign.Center,
                 fontSize = 20.sp,
-                modifier = modifier
+                modifier = Modifier
                     .weight(4f, true)
             )
 
             IconButton(onClick = { /*TODO*/ },
-                modifier = modifier
+                modifier = Modifier
                     .padding(0.dp)
             ) {
                 Icon(
                     painter = painterResource(id = R.drawable.add_circle_black_36dp),
                     contentDescription = null,
-                    modifier = modifier,
+                    modifier = Modifier,
                     tint = MaterialTheme.colorScheme.primary
                 )
             }
